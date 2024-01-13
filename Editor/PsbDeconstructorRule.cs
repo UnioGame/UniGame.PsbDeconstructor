ce724f86edb4f075d308ed0a38301fcf2d4eb406
Mefodei <mefodei.link@gmail.com>
2024-01-13 12:52:14 +0700

    - fix dependency errors

--- Editor/PsbDeconstructorRule.cs
+++ Editor/PsbDeconstructorRule.cs
@@ -1,9 +1,12 @@
 ﻿namespace UniGame.Tools.PsbDeconstructor
 {
     using System;
-    using Sirenix.OdinInspector;
     using UniModules.UniGame.CoreModules.UniGame.GraphicsTools.Editor;
 
+#if ODIN_INSPECTOR
+    using Sirenix.Utilities.Editor;
+#endif
+    
     [Serializable]
     public class PsbDeconstructorRule
     {
@@ -11,7 +14,9 @@
         public string TargetPath                    = string.Empty;
         public bool UseCustomSpriteSettings         = false;
 
+#if ODIN_INSPECTOR
         [Title("Importer Settings"), HideLabel, ShowIf("UseCustomSpriteSettings")]
+#endif
         public TextureImporterSettingsAdapter ImportSettings = new TextureImporterSettingsAdapter();
     }
 }
--- Editor/PsbDeconstructorSettings.cs
+++ Editor/PsbDeconstructorSettings.cs
@@ -3,9 +3,12 @@
     using UniModules.UniGame.Core.Editor.EditorProcessors;
     using UnityEngine;
     using System.Collections.Generic;
-    using Sirenix.OdinInspector;
     using UniModules.UniGame.CoreModules.UniGame.GraphicsTools.Editor;
 
+#if ODIN_INSPECTOR
+    using Sirenix.Utilities.Editor;
+#endif
+    
     [GeneratedAssetInfo("Assets/UniGame.Generated/PsbDeconstructor/Editor")]
     public class PsbDeconstructorSettings : GeneratedAsset<PsbDeconstructorSettings>
     {
@@ -16,25 +19,35 @@
         #endregion
 
         [SerializeField]
+#if ODIN_INSPECTOR
         [FolderPath, TitleGroup("Paths")]
+#endif        
         [Tooltip("If PSB file is moved after unpacking, folders contained inside the root directory will be added to the new path.")]
         public string PSBRootDirectory = "Assets/GameContent/Art";
 
         [SerializeField]
+#if ODIN_INSPECTOR
         [FolderPath, TitleGroup("Paths")]
+#endif
         [Tooltip("Path to directory where PSB file will be moved after unpacking. If empty, PSB file won't be moved.")]
         public string PathToMovePSB;
 
         [SerializeReference]
+#if ODIN_INSPECTOR
         [TitleGroup("Additional Settings")]
+#endif
         private PsbUnPacker _defaultUnPacker;
 
         [SerializeField]
+#if ODIN_INSPECTOR
         [TitleGroup("Rules"), LabelText("Rules")]
+#endif
         public List<PsbDeconstructorRule> RuleSettings = new List<PsbDeconstructorRule>();
 
         [SerializeField]
+#if ODIN_INSPECTOR
         [Title("Default Importer Settings"), HideLabel]
+#endif
         public TextureImporterSettingsAdapter ImportSettings = new TextureImporterSettingsAdapter();
 
         public PsbUnPacker DefaultUnPacker
